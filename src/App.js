d2eb3f546df5f9f2dd3f4953c39a2c392fcb18d4
jordynmcginnis <jordynm4@gmail.com>
2018-09-11 16:02:01 -0600

    removed logo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+          <h1 className="app-title">Codenames</h1>
+        <div className="app-intro">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
