6a6570a5a5f1901618716c0e3530e04a1c005ed9
jordynmcginnis <jordynm4@gmail.com>
2018-09-13 10:41:31 -0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import ReactRouter, { Route, BrowserRouter as Router } from 'react-router-dom';
-import CreateGame from './CreateGame.js';
-import {fetchGames} from './api/index.js';
 import Home from './Home.js';
+
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="app">
-        <h1 className="app-title">Codenames</h1>
-        <Route exact path='/' component={Home}/>
-      </div>
+        <div className="app">
+          <h1 className="app-title">Codenames</h1>
+          <Route exact path='/' component={Home}/>
+        </div>
       </Router>
     );
   }
