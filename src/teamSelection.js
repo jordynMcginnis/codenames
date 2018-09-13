b7e15441291e83388913ad178b905b5b74ca1fa1
jordynmcginnis <jordynm4@gmail.com>
2018-09-13 15:21:44 -0600

    renders players to screen

--- src/teamSelection.js
+++ src/teamSelection.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import {submitName} from './api/index.js';
+import { firebasedb } from './utils/config.js';
 
 class TeamSelection extends Component {
   constructor(props) {
     super(props);
     this.state = {
       render: 'name',
-      name: false
+      name: false,
+      players: {}
     }
     this.submitName = this.submitName.bind(this);
     this.updateInput = this.updateInput.bind(this);
+  }
+  componentDidMount () {
+    let that = this;
+    let games = firebasedb.ref('/games/' + this.props.id + '/players');
+    games.on('value', function (snapshot) {
+      let value = snapshot.val();
+      that.setState(() => ({players: value}));
+    })
   }
   submitName () {
     submitName(this.state.name, this.props.id);
@@ -30,8 +40,13 @@ class TeamSelection extends Component {
               <button onClick ={this.submitName}>Submit</button>
             </div>
           : <div>
-                <div className='blue-team'> jordyn </div>
-                <div className='red-team'> jordyn </div>
+                {Object.keys(this.state.players).map((player) => {
+                  if(player[0] === 'b'){
+                    return <div className='blue-team' key={player} >{this.state.players[player]} </div>
+                  } else {
+                    return <div className='red-team' key={player}> {this.state.players[player]} </div>
+                  }
+                })}
                 <button className='switch'> SWITCH TEAMS</button>
             </div>
         }
