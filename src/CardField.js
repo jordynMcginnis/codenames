c81b6178a1419da360c2c731f46564d2da9e92d8
jordynmcginnis <jordynm4@gmail.com>
2018-09-16 14:05:29 -0600

    cardfeild maps over data rendering each card

--- src/CardField.js
+++ src/CardField.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Card from './Card.js';
 
 class CardField extends Component {
   render() {
     return (
       <div className="board">
-
+        {Object.keys(this.props.data).map(() => <Card/>)}
       </div>
     );
   }
