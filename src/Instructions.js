c269df0a3a2e30417f4d0eb2b5d9248e36242ac8
jordynmcginnis <jordynm4@gmail.com>
2019-01-25 16:22:41 -0700

    added indention

--- src/Instructions.js
+++ src/Instructions.js
@@ -5,26 +5,36 @@ class Instructions extends Component {
     return (
       <div>
         <ul className='rules'>
-          <li><GoOrganization className='icon'/>Join existing game below or create game above.You need at least four
-            players to play. </li>
-          <li><GoGistSecret className='icon'/>
+          <li>
+            <GoOrganization className='icon'/>
+            Join existing game below or create game above.You need at least four
+            players to play.
+          </li>
+          <li>
+            <GoGistSecret className='icon'/>
             Spymasters know the secret identities of 25 agents. Their teammates know the agents only by their
             codenames.
           </li>
-          <li><GoPerson className='icon'/>
-          Spymasters take turns giving one-word clues. A clue may relate to multiple words on the screen.
+          <li>
+            <GoPerson className='icon'/>
+            Spymasters take turns giving one-word clues. A clue may relate to multiple words on the screen.
           </li>
-          <li><GoStar className='icon'/> The field
-operatives try to guess which words their spymaster meant. When a field operative chooses a word, the
-game reveals its secret identity.</li>
-          <li><GoPrimitiveDot className='icon'/>
-           If the field operatives guess correctly, they may continue guessing,
-until they run out of ideas for the given clue or until they hit a wrong person.
+          <li>
+            <GoStar className='icon'/>
+            The field
+            operatives try to guess which words their spymaster meant. When a field operative chooses a word, the
+            game reveals its secret identity.
           </li>
-          <li><GoZap className='icon'/>
-          Then it is the other team's
-turn to give a clue and guess.
-           The first team to contact all their agents wins the game.
+          <li>
+            <GoPrimitiveDot className='icon'/>
+            If the field operatives guess correctly, they may continue guessing,
+            until they run out of ideas for the given clue or until they hit a wrong person.
+          </li>
+          <li>
+            <GoZap className='icon'/>
+            Then it is the other team's
+            turn to give a clue and guess.
+            The first team to contact all their agents wins the game.
           </li>
         </ul>
       </div>
